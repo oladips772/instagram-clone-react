e708d0d065a2d00770e8f0d9c9ceaacdd54b907c
oladips <oladips200@gmail.com>
2022-10-05 10:29:25 +0000

    g

---
+++ src/components/Header.js
@@ -0,0 +1,17 @@
+/** @format */
+import React from "react";
+
+function Header() {
+  return (
+    <div className="flex items-center justify-around h-[55px] shadow">
+      <img
+        src="https://logos-download.com/wp-content/uploads/2016/03/Instagram_Logo_2016.png"
+        className="h-[30px] object-contain cursor-pointer"
+        alt=""
+      />
+      <div></div>
+    </div>
+  );
+}
+
+export default Header;
